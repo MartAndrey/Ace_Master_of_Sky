d01faf6fb1bedf3c5636995c6fe2527fb1f2edfb
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-10-09 16:51:23 -0500

    Improving enemy movement

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -8,16 +8,26 @@ public class EnemyMove : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float speed;
- 
+
     Rigidbody2D rb;
+    Vector2 vectorDirection;
+    float angleOffset = 90;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        vectorDirection = (target.position - transform.position).normalized;
+    }
+
     void FixedUpdate()
     {
-        rb.velocity = (target.position - transform.position).normalized * speed;
+        float angle = Mathf.Atan2(vectorDirection.y, vectorDirection.x) * Mathf.Rad2Deg;
+
+        rb.velocity = vectorDirection * speed;
+        rb.rotation = angle - angleOffset;
     }
 }
