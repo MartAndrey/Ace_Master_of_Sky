9a1080259a69cf6915b1bd8d8a9c689d23012014
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-10-12 16:29:47 -0500

    JSON: Weather Change

--- Assets/Scripts/Weather/WeatherChange.cs
+++ Assets/Scripts/Weather/WeatherChange.cs
@@ -6,13 +6,55 @@ using LitJson;
 
 public class WeatherChange : MonoBehaviour
 {
+    [SerializeField] DigitalRuby.RainMaker.RainScript2D rainMaker;
+
     int currentWeather;
 
     void Start()
     {
         StartCoroutine(GetWeather());
     }
-    
+
+    void WeatherChanger()
+    {
+
+        if (currentWeather >= 200 && currentWeather < 300)
+        {
+            // Storm
+            rainMaker.RainIntensity += 1;
+        }
+        else if (currentWeather >= 300 && currentWeather < 400)
+        {
+            // Drizzle
+            rainMaker.RainIntensity += 0.2f;
+        }
+        else if (currentWeather >= 400 && currentWeather < 500)
+        {
+            // Rain
+            rainMaker.RainIntensity += 0.55f;
+        }
+        else if (currentWeather >= 500 && currentWeather < 600)
+        {
+            // Rain
+            rainMaker.RainIntensity += 0.7f;
+        }
+        else if (currentWeather >= 700 && currentWeather < 800)
+        {
+            // Fog
+            rainMaker.RainIntensity += 0.1f;
+        }
+        else if (currentWeather > 800)
+        {
+            // Clouds
+            rainMaker.RainIntensity += 0.1f;
+        }
+        else if (currentWeather == 800)
+        {
+            // ClearSky
+            rainMaker.gameObject.SetActive(false);
+        }
+    }
+
     IEnumerator GetWeather()
     {
         UnityWebRequest www = UnityWebRequest.Get("TokenWeather");
@@ -21,12 +63,16 @@ public class WeatherChange : MonoBehaviour
         if (www.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError(www.error);
+            currentWeather = 800;
         }
         else
         {
             JsonData jsonData = JsonMapper.ToObject(www.downloadHandler.text);
-            currentWeather = (int ) jsonData["weather"] [0] ["id"];
-            Debug.Log(currentWeather);
+            currentWeather = (int)jsonData["weather"][0]["id"];
         }
+
+        Debug.Log(currentWeather);
+        WeatherChanger();
+        StopCoroutine(GetWeather());
     }
 }
